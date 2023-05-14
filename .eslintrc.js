2a102c4b4f78d44d7c29d81b95c54675b9c4916e
416rehman <15690606+416rehman@users.noreply.github.com>
2023-05-14 10:32:46 -0400

    Add Pino logger and update ESLint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: "airbnb-base",
+  extends: "eslint:recommended",
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
