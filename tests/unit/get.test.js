a16a3e3c07c94587fa36b2258f7b3458e42992c5
416rehman <15690606+416rehman@users.noreply.github.com>
2023-06-01 16:36:37 -0400

    Add tests for GET requests to /v1/fragments, /v1/fragments/:id and /v1/fragments/:id/info endpoints.

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -1,8 +1,20 @@
 // tests/unit/get.test.js
-
 const request = require("supertest");
-
 const app = require("../../src/app");
+const db = require("../../src/db/inmemoryDB.js");
+const {
+  createSuccessResponse,
+  createErrorResponse,
+} = require("../../src/response");
+const { convert } = require("../../src/utils/helpers");
+
+const authEmail = "user1@email.com";
+const authPassword = "password1";
+
+const fragmentBody = "sample body";
+const fragmentType = "text/markdown";
+const ownedFragment = db.create(fragmentBody, fragmentType, "user1@email.com");
+const otherFragment = db.create(fragmentBody, fragmentType, "user2@email.com");
 
 describe("GET /v1/fragments", () => {
   // If the request is missing the Authorization header, it should be forbidden
@@ -16,15 +28,117 @@ describe("GET /v1/fragments", () => {
       .auth("invalid@email.com", "incorrect_password")
       .expect(401));
 
-  // Using a valid username/password pair should give a success result with a .fragments array
-  test("authenticated users get a fragments array", async () => {
-    const res = await request(app)
-      .get("/v1/fragments")
-      .auth("user1@email.com", "password1");
-    expect(res.statusCode).toBe(200);
-    expect(res.body.status).toBe("ok");
-    expect(Array.isArray(res.body.fragments)).toBe(true);
+  // GET /v1/fragments/
+  describe("GET /v1/fragments/", () => {
+    test("should return an array of owned fragments' IDs when not expanded", async () => {
+      const res = await request(app)
+        .get("/v1/fragments/")
+        .auth(authEmail, authPassword)
+        .expect(200);
+
+      expect(res.body).toEqual(
+        createSuccessResponse({ fragments: [ownedFragment.id] })
+      );
+    });
+
+    test("should return an array of owned fragments' metadata when expanded", async () => {
+      const res = await request(app)
+        .get("/v1/fragments/?expand=1")
+        .auth(authEmail, authPassword)
+        .expect(200);
+
+      expect(res.body).toEqual(
+        createSuccessResponse({ fragments: [ownedFragment] })
+      );
+    });
   });
 
-  // TODO: we'll need to add tests to check the contents of the fragments array later
+  // GET /v1/fragments/:id
+  describe("GET /v1/fragments/:id", () => {
+    test("should return 404 if fragment does not exist", async () => {
+      const nonExistentId = "nonexistentid";
+      const res = await request(app)
+        .get(`/v1/fragments/${nonExistentId}`)
+        .auth(authEmail, authPassword)
+        .expect(404);
+
+      expect(res.body).toEqual(createErrorResponse(404, "Fragment not found"));
+    });
+
+    test("should return the fragment data if it exists", async () => {
+      const res = await request(app)
+        .get(`/v1/fragments/${ownedFragment.id}`)
+        .auth(authEmail, authPassword)
+        .expect(200);
+
+      expect(res.text).toEqual(fragmentBody);
+    });
+
+    test("should not return the data if the fragment is not owned by the user", async () => {
+      const res = await request(app)
+        .get(`/v1/fragments/${otherFragment.id}`)
+        .auth(authEmail, authPassword)
+        .expect(404);
+
+      expect(res.body).toEqual(createErrorResponse(404, "Fragment not found"));
+    });
+
+    test("should return 415 if unsupported conversion requested", async () => {
+      const extension = "mp4";
+      const res = await request(app)
+        .get(`/v1/fragments/${ownedFragment.id}.${extension}`)
+        .auth(authEmail, authPassword)
+        .expect(415);
+
+      // the error message should contain "Unsupported conversion"
+      expect(res.body.error.message).toMatch(/Unsupported conversion/);
+    });
+
+    test("should return the converted fragment data if conversion is supported", async () => {
+      const supportedExtension = "html";
+      const res = await request(app)
+        .get(`/v1/fragments/${ownedFragment.id}.${supportedExtension}`)
+        .auth(authEmail, authPassword)
+        .expect(200);
+
+      const conversionResult = convert(
+        fragmentBody,
+        fragmentType,
+        supportedExtension
+      );
+      if (conversionResult.success) {
+        expect(res.text).toEqual(conversionResult.data);
+      } else {
+        expect(res.body.error.message).toMatch(/Unsupported conversion/);
+      }
+    });
+  });
+
+  // GET /v1/fragments/:id/info
+  describe("GET /v1/fragments/:id/info", () => {
+    test("should return 404 if fragment does not exist", async () => {
+      const nonExistentId = "nonexistentid";
+      const res = await request(app)
+        .get(`/v1/fragments/${nonExistentId}/info`)
+        .auth(authEmail, authPassword)
+        .expect(404);
+
+      expect(res.body).toEqual(createErrorResponse(404, "Fragment not found"));
+    });
+
+    test("should return the owned fragment info if it exists", async () => {
+      const res = await request(app)
+        .get(`/v1/fragments/${ownedFragment.id}/info`)
+        .auth(authEmail, authPassword)
+        .expect(200);
+
+      expect(res.body).toEqual(
+        createSuccessResponse({
+          fragment: {
+            metadata: ownedFragment,
+          },
+        })
+      );
+    });
+  });
 });
