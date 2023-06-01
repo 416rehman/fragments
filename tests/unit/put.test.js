3e63bbcb808ecc81b4cdca63e7dfa9645c2dc8ab
416rehman <15690606+416rehman@users.noreply.github.com>
2023-06-01 17:15:14 -0400

    added auth checks

--- tests/unit/put.test.js
+++ tests/unit/put.test.js
@@ -17,6 +17,17 @@ const otherFragment = db.create(fragmentBody, fragmentType, "user2@email.com");
 const updatedFragmentBody = "updated body";
 
 describe("PUT /v1/fragments/:id", () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test("unauthenticated requests are denied", () =>
+    request(app).put(`/v1/fragments/${ownedFragment.id}`).expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test("incorrect credentials are denied", () =>
+    request(app)
+      .put(`/v1/fragments/${ownedFragment.id}`)
+      .auth("invalid@email.com", "incorrect_password")
+      .expect(401));
+
   test("should return 404 if fragment does not exist", async () => {
     const nonExistentId = "nonexistentid";
     const res = await request(app)
