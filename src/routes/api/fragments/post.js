5e263b93e551aecfbe262b7f085bbdb3b2764c3f
416rehman <15690606+416rehman@users.noreply.github.com>
2023-06-08 05:03:31 -0400

    set location header

--- src/routes/api/fragments/post.js
+++ src/routes/api/fragments/post.js
@@ -1,43 +1,45 @@
 const db = require("../../../db/inmemoryDB.js");
 const {
-  isContentTypeSupported,
-  conversionTable,
+    isContentTypeSupported,
+    conversionTable,
 } = require("../../../utils/helpers");
 /**
  * Get a list of fragments for the current user
  */
 const {
-  createSuccessResponse,
-  createErrorResponse,
+    createSuccessResponse,
+    createErrorResponse,
 } = require("../../../response");
 
 module.exports = (req, res) => {
-  const type = req.headers["content-type"];
-  const binaryData = req.body;
+    const type = req.headers["content-type"];
+    const binaryData = req.body;
 
-  console.log('POST /v1/fragments', {type, binaryData});
+    console.log('POST /v1/fragments', {type, binaryData});
 
-  if (!binaryData || binaryData.length === 0) {
-    res.status(400).send(createErrorResponse(400, "Missing body"));
-  }
+    if (!binaryData || binaryData.length === 0) {
+        res.status(400).send(createErrorResponse(400, "Missing body"));
+    }
 
-  if (!isContentTypeSupported(type)) {
-    const validTypes = Object.keys(conversionTable).join(", ");
-    res
-      .status(415)
-      .send(
-        createErrorResponse(
-          415,
-          "Unsupported Media Type. Valid types are: " + validTypes
-        )
-      );
-  }
+    if (!isContentTypeSupported(type)) {
+        const validTypes = Object.keys(conversionTable).join(", ");
+        res
+            .status(415)
+            .send(
+                createErrorResponse(
+                    415,
+                    "Unsupported Media Type. Valid types are: " + validTypes
+                )
+            );
+    }
 
-  const fragment = db.create(binaryData, type, req.user);
-  if (!fragment) {
-    //Server error
-    res.status(500).send(createErrorResponse(500, "Failed to create fragment"));
-  }
+    const fragment = db.create(binaryData, type, req.user);
+    if (!fragment) {
+        //Server error
+        res.status(500).send(createErrorResponse(500, "Failed to create fragment"));
+    }
 
-  res.status(201).send(createSuccessResponse({ fragment }));
+    res.set("Location", `/v1/fragments/${fragment.id}`);
+
+    res.status(201).send(createSuccessResponse({fragment}));
 };
